2998d62556aa8d17dbc436fd0b25a050aa340e40
Pratik <pratikyadav3949@gmail.com>
2026-04-03 22:56:56 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+// server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
