cdfcae768cd1e0c4ffe2f81fef3416cea4f6ca94
Pratik <pratikyadav3949@gmail.com>
2026-04-12 21:19:37 +0530

    start

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,17 @@
-// server.js
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
---
+++ frontend/src/Components/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-6">
+      <h1 className="text-3xl font-bold">
+        Hello from JSX 🚀
+      </h1>
+      <p>This is a React component.</p>
+    </div>
+  )
+}
+
+export default App
