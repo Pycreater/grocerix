3b6bb3bb19a67aea5d2683eb43176f2be15969ff
Pratik <pratikyadav3949@gmail.com>
2026-04-12 22:11:53 +0530

    user auth done

---
+++ backend/controllers/userController.js
@@ -0,0 +1,59 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// REGISTER
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // check user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await user.save();
+
+    res.status(201).json({
+      message: "User registered",
+      user: savedUser,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// LOGIN
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    res.json({
+      message: "Login successful",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 // DB Connection
 connectDB();
